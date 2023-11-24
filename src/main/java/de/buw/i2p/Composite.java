7e6b8ed5fd2479dcfda813ae3746de0d8dc4ac20
ANNI90123 <ajngit@mail.n-ithing.de>
2023-11-24 18:19:26 +0100

    Parametrize the generate_2 function and update generate_0, generate_1

--- src/main/java/de/buw/i2p/Composite.java
+++ src/main/java/de/buw/i2p/Composite.java
@@ -26,7 +26,7 @@ public class Composite {
         Kreis base = new Kreis(center_x, center_y, diameter/2, false);
         comp_container.add(base);
         int num = (int)(4* Math.random());
-        generate_0(num_segments);
+        generate_2(num_segments);
         /*
         switch(num){
             case(0):
@@ -56,12 +56,12 @@ public class Composite {
 
         float angle = (float)(2* Math.PI/ num_segments);
         for (int i = 0 ; i < num_segments; i++){
-            element_center_x = 350.0f + (float)(Math.cos(i * angle)*radius_small);
-            element_center_y = 350.0f + (float)(Math.sin(i * angle)*radius_small);
+            element_center_x = center_x + (float)(Math.cos(i * angle)*radius_small);
+            element_center_y = center_y + (float)(Math.sin(i * angle)*radius_small);
             comp_container.add(new Kreis(element_center_x, element_center_y, element_radius_small, false));
             //comp_container.add(new Quadrat(element_center_x, element_center_y, 30.0f, 30.0f , false));
-            element_center_x = 350.0f + (float)(Math.cos(i * angle)*radius_big);
-            element_center_y = 350.0f + (float)(Math.sin(i * angle)*radius_big);
+            element_center_x = center_x + (float)(Math.cos(i * angle)*radius_big);
+            element_center_y = center_y + (float)(Math.sin(i * angle)*radius_big);
             comp_container.add(new Kreis(element_center_x, element_center_y, element_radius_big, false));
             //comp_container.add(new Quadrat(element_center_x, element_center_y, 50.0f, 50.0f , false));
         }
@@ -75,23 +75,23 @@ public class Composite {
         float element_radius = diameter/4.0f;
         float angle = (float)(2* Math.PI/ num_segments);
         for (int i = 0 ; i < num_segments; i++){
-            element_center_x = 350.0f + (float)(Math.cos(i * angle)* radius);
-            element_center_y = 350.0f + (float)(Math.sin(i * angle)* radius);
+            element_center_x = center_x + (float)(Math.cos(i * angle)* radius);
+            element_center_y = center_y + (float)(Math.sin(i * angle)* radius);
             comp_container.add(new Kreis(element_center_x, element_center_y, element_radius, true));
             //comp_container.add(new Quadrat(element_center_x, element_center_y, 2 * element_radius * (float)Math.sin(Math.PI/4.0),2* element_radius * (float)Math.sin(Math.PI/4.0), true));
         }
     }
 
     public void generate_2(int num_segments){
-        float center_x;
-        float center_y;
+        float element_center_x;
+        float element_center_y;
         float angle = (float)(2* Math.PI/ num_segments);
-        float radius = 125.0f;
+        float radius = diameter/2 * 0.45f;
         for (int i = 0 ; i < num_segments; i++){
-            center_x = 350.0f + (float)(Math.cos(i * angle)* radius);
-            center_y = 350.0f + (float)(Math.sin(i * angle)*radius);
-            comp_container.add(new Kreis(center_x, center_y, radius, true));
-            //comp_container.add(new Quadrat(center_x, center_y, 2 * radius * (float)Math.sin(Math.PI/4.0),2* radius * (float)Math.sin(Math.PI/4.0),  true));
+            element_center_x = center_x + (float)(Math.cos(i * angle)* radius);
+            element_center_y = center_y + (float)(Math.sin(i * angle)*radius);
+            comp_container.add(new Kreis(element_center_x, element_center_y, radius, true));
+            //comp_container.add(new Quadrat(element_center_x, element_center_y, 2 * radius * (float)Math.sin(Math.PI/4.0),2* radius * (float)Math.sin(Math.PI/4.0),  true));
         }
     }
 

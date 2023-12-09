02a229e7063b0d6ec253e9c485869963f1bbb2cb
unknown <adam.alshourbagi@gmail.com>
2023-12-09 17:17:35 +0100

    added classes and change Point2D to Vec2

--- src/main/java/de/buw/i2p/FraktalDreieck.java
+++ src/main/java/de/buw/i2p/FraktalDreieck.java
@@ -4,32 +4,31 @@ import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.canvas.*;
 import java.awt.*;
-public class FractalTriangle extends Composite{
-    private Point2D corner_0;
-    private Point2D corner_1;
-    private Point2D corner_2;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+public class FraktalDreieck extends Composite{
+    private Vector2D centerPoint;
+    private float radius; //Distanz von der Mitte zu einem Punkt
     private Color canvas_color; //Linien Farbe des Kreises für den Canvas
     private java.awt.Color buffer_color;    //Linien Farbe des Kreises für das zu speichernde Bild
-    private int recursion_Depth;
+    private int recursionDepth;
 
-    public FractalTriangle(Point2D corner_0, Point2D corner_1, Point2D corner_2, int recursion_Depth){
-        this.corner_0 = corner_0;
-        this.corner_1 = corner_1;
-        this.corner_2 = corner_2;
-        this.recursion_Depth = recursion_Depth;
+    public FraktalDreieck(Vector2D centerPoint, float radius,int recursionDepth){
+        this.centerPoint =  centerPoint;
+        this.recursionDepth = recursionDepth;
         canvas_color = Color.BLACK;
         buffer_color = java.awt.Color.BLACK;
     }
 
     //das Fraktal wird auf dem GraphicsContext für den Canvas gezeichnet
     public void print(GraphicsContext gc_canvas){
-        double[] x_points = {corner_0.getX(), corner_1.getX(), corner_2.getX()};
+       /* double[] x_points = {corner_0.getX(), corner_1.getX(), corner_2.getX()};
         double[] y_points = {corner_0.getY(), corner_1.getY(), corner_2.getY()};
         gc_canvas.setStroke(canvas_color);
         gc_canvas.strokePolygon(x_points, y_points, 3);
 
         //todo create subfractals and recursively call the print method on them
-        /*
+
         Fractal1 left_sub_Fractal = new Fractal1(corner_0,
                                             new Point2D(corner_0.getX() + (corner_1.getX() - corner_0.getX())/2,corner_0.getY() + (corner_1.getY() - corner_0.getY())/2),
                                             new Point2D(corner_0.getX() + (corner_2.getX() - corner_0.getX())/2,corner_0.getY() + (corner_2.getY() - corner_0.getY())/2),
---
+++ src/main/java/de/buw/i2p/FraktalKreis.java
@@ -0,0 +1,38 @@
+package de.buw.i2p;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import java.awt.*;
+
+import org.apache.commons.math3.geometry.Vector;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+
+
+public class FraktalKreis extends Composite {
+    private Vector2D centerPoint;
+    private float radius;
+    private Color canvas_color; //Linien Farbe des Kreises für den Canvas
+    private java.awt.Color buffer_color;//Linien Farbe des Kreises für das zu speichernde Bild
+    private int recursionDepth;
+
+    public FraktalKreis(Vector2D centerPoint, float radius, int recursionDepth) {
+        this.centerPoint = centerPoint;
+        this.radius = radius;
+        this.recursionDepth = recursionDepth;
+        canvas_color = Color.BLACK;
+        buffer_color = java.awt.Color.BLACK;
+    }
+
+    public void print(GraphicsContext gc_canvas) {
+
+        //todo create subfractals and recursively call the print method on them
+
+    }
+
+    //das Fraktal wird auf dem GraphicsContext, der später gespeichert wird, gezeichnet
+    public void save(Graphics2D gc_buffer) {
+
+    }
+}

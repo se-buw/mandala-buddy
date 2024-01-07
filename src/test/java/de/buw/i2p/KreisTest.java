368164fbaa72a716a9e59009d71e600e3aa24bd5
Patricia Mangold <patricia.mangold@uni-weimar.de>
2024-01-07 12:41:20 +0100

    Kreis Test Saves added

--- src/test/java/de/buw/i2p/KreisTest.java
+++ src/test/java/de/buw/i2p/KreisTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import javafx.scene.paint.Color;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
@@ -53,6 +55,48 @@ class KreisTest {
 	        // Assertions
 	        assertEquals(Color.BLACK, gcCanvas.getStroke());
 	        assertEquals(Color.WHITE, gcCanvas.getFill());
+
+	        // You can add more assertions based on your requirements
+	    }
+	   @Test
+	    void testSaveTransparentKreis() {
+	        Vector2D center = new Vector2D(0, 0);
+	        double radius = 5;
+	        boolean transparent = true;
+	        Color canvasBackground = Color.WHITE;
+	        java.awt.Color bufferBackground = java.awt.Color.BLACK;
+
+	        Kreis kreis = new Kreis(center, radius, transparent, canvasBackground, bufferBackground);
+
+	        BufferedImage bufferedImage = new BufferedImage(400, 400, BufferedImage.TYPE_INT_ARGB);
+	        Graphics2D gcBuffer = bufferedImage.createGraphics();
+
+	        kreis.save(gcBuffer);
+
+	        // Verify that the correct methods were called on the Graphics2D
+	        assertEquals(java.awt.Color.BLACK, gcBuffer.getColor());
+
+	        // You can add more assertions based on your requirements
+	    }
+
+	    @Test
+	    void testSaveFilledKreis() {
+	        Vector2D center = new Vector2D(0, 0);
+	        double radius = 5;
+	        boolean transparent = false;
+	        Color canvasBackground = Color.WHITE;
+	        java.awt.Color bufferBackground = java.awt.Color.BLACK;
+
+	        Kreis kreis = new Kreis(center, radius, transparent, canvasBackground, bufferBackground);
+
+	        BufferedImage bufferedImage = new BufferedImage(400, 400, BufferedImage.TYPE_INT_ARGB);
+	        Graphics2D gcBuffer = bufferedImage.createGraphics();
+
+	        kreis.save(gcBuffer);
+
+	        // Verify that the correct methods were called on the Graphics2D
+	        assertEquals(java.awt.Color.BLACK, gcBuffer.getColor());
+	        assertEquals(java.awt.Color.WHITE, gcBuffer.getBackground());
 
 	        // You can add more assertions based on your requirements
 	    }
